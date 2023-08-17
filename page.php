2e3173869ec5dd0f753170f64e2d7a47b5affb82
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-08-17 04:47:12 +0300

    just fixing the code intendaion

--- page.php
+++ page.php
@@ -398,83 +398,31 @@ $contact_website = get_post_meta( get_the_ID(), 'contact_website', true);
 			?>
 		  <h3 class="subtitle"><?php echo $service_title;?></h3>
 		  <div class="row vertical-line">
-		  <?php 
-		$services_types = get_post_meta( get_the_ID(), 'services_type', true );
-		foreach ( (array) $services_types as $key => $services_type ) {
-			$services_type_name = $services_type_description = $services_type_image = '';
-			if ( isset( $services_type['services_type_name'] ) ) {
-				$services_type_name = esc_html( $services_type['services_type_name'] );
-			}
-			if ( isset( $services_type['services_type_description'] ) ) {
-				$services_type_description = wpautop( $services_type['services_type_description'] );
-			}
-			if ( isset( $services_type['services_type_image'] ) ) {
-				$services_type_image = esc_html( $services_type['services_type_image']);
-			}
-			
-		
-			echo '
-			<div class="col-md-6">
-			  <div class="services-box">
-				<img src="'.$services_type_image.'" alt="/">
-				<div class="services-content">
-				  <h6 class="services-head">'.$services_type_name.'</h6>
-				  <p class="services-description mb-0">'.$services_type_description.'</p>
-				</div>
-			  </div>
-			</div>';
-		}
-	  ?>
-			<!-- Item 01 -->
-			<div class="col-md-6">
-			  <div class="services-box">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/colorfull/prototype.png" alt="/">
-				<div class="services-content">
-				  <h6 class="services-head">Web Design</h6>
-				  <p class="services-description mb-0">In a professional context it often happens that private or corporate clients corder a publication to be made.</p>
-				</div>
-			  </div>
-			</div>
-			<!-- Item 02 -->
-			<div class="col-md-6">
-			  <div class="services-box">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/colorfull/prototype.png" alt="/">
-				<div class="services-content">
-				  <h6 class="services-head">Logo Design</h6>
-				  <p class="services-description mb-0">In a professional context it often happens that private or corporate clients corder a publication to be made.</p>
-				</div>
-			  </div>
-			</div>
-			<!-- Item 03 -->
-			<div class="col-md-6">
-			  <div class="services-box">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/colorfull/layer.png" alt="/">
-				<div class="services-content">
-				  <h6 class="services-head">Web Development</h6>
-				  <p class="services-description mb-0">In a professional context it often happens that private or corporate clients corder a publication to be made.</p>
-				</div>
-			  </div>
-			</div>
-			<!-- Item 04 -->
-			<div class="col-md-6">
-			  <div class="services-box">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/colorfull/movie-frame.png" alt="/">
-				<div class="services-content">
-				  <h6 class="services-head">Short Animations</h6>
-				  <p class="services-description mb-0">In a professional context it often happens that private or corporate clients corder a publication to be made.</p>
-				</div>
-			  </div>
-			</div>
-			<!-- Item 05 -->
-			<div class="col-md-6">
-			  <div class="services-box">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/colorfull/bar-graph.png" alt="/">
-				<div class="services-content">
-				  <h6 class="services-head">Advetising</h6>
-				  <p class="services-description mb-0">In a professional context it often happens that private or corporate clients corder a publication to be made.</p>
-				</div>
-			  </div>
-			</div>
+			<?php 
+				$services_types = get_post_meta( get_the_ID(), 'services_type', true );
+				foreach ( (array) $services_types as $key => $services_type ) {
+					$services_type_name = $services_type_description = $services_type_image = '';
+					if ( isset( $services_type['services_type_name'] ) ) {
+						$services_type_name = esc_html( $services_type['services_type_name'] );
+					}
+					if ( isset( $services_type['services_type_description'] ) ) {
+						$services_type_description = wpautop( $services_type['services_type_description'] );
+					}
+					if ( isset( $services_type['services_type_image'] ) ) {
+						$services_type_image = esc_html( $services_type['services_type_image']);
+					}
+						echo '
+						<div class="col-md-6">
+							<div class="services-box">
+								<img src="'.$services_type_image.'" alt="/">
+								<div class="services-content">
+									<h6 class="services-head">'.$services_type_name.'</h6>
+									<p class="services-description mb-0">'.$services_type_description.'</p>
+								</div>
+							</div>
+						</div>';
+				}
+			?>
 		  </div>
 		</div>
 	  </div>
@@ -488,38 +436,36 @@ $contact_website = get_post_meta( get_the_ID(), 'contact_website', true);
 		  <h3 class="subtitle"><?php echo $education_title;?></h3>
 		  <div class="row vertical-line">
 			<!--  Item 01  -->
-		  <?php 
-		$education_types = get_post_meta( get_the_ID(), 'education_type', true );
-		foreach ( (array) $education_types as $key => $education_type ) {
-			$education_type_name = $education_institution = $education_type_description = $education_year = '';
-			if ( isset( $education_type['education_type_name'] ) ) {
-				$education_type_name = esc_html( $education_type['education_type_name'] );
-			}
-			if ( isset( $education_institution['education_institution'] ) ) {
-				$education_institution = wpautop( $education_type['education_institution'] );
-			}
-			if ( isset( $education_type['education_year'] ) ) {
-				$education_year = esc_html( $education_type['education_year']);
-			}
-			if ( isset( $education_type['education_type_description'] ) ) {
-				$education_type_description = esc_html( $education_type['education_type_description']);
-			}
-			
-		
-			echo '
-			<div class="col-md-6">
-			  <div class="timeline-box">
-				<div class="time-line-header">
-				  <p class="timeline-year">'.$education_year.'</p>
-				  <h6 class="timeline-title">'.$education_type_name.'</h6>
-				</div>
-				<div class="timeline-content">
-				  <p>'.$education_type_description.'</p>
-				</div>
-			  </div>
-			</div>';
-		}
-	  ?>
+		  	<?php 
+				$education_types = get_post_meta( get_the_ID(), 'education_type', true );
+				foreach ( (array) $education_types as $key => $education_type ) {
+					$education_type_name = $education_institution = $education_type_description = $education_year = '';
+					if ( isset( $education_type['education_type_name'] ) ) {
+						$education_type_name = esc_html( $education_type['education_type_name'] );
+					}
+					if ( isset( $education_institution['education_institution'] ) ) {
+						$education_institution = wpautop( $education_type['education_institution'] );
+					}
+					if ( isset( $education_type['education_year'] ) ) {
+						$education_year = esc_html( $education_type['education_year']);
+					}
+					if ( isset( $education_type['education_type_description'] ) ) {
+						$education_type_description = esc_html( $education_type['education_type_description']);
+					}
+					echo '
+					<div class="col-md-6">
+						<div class="timeline-box">
+							<div class="time-line-header">
+							<p class="timeline-year">'.$education_year.'</p>
+							<h6 class="timeline-title">'.$education_type_name.'</h6>
+							</div>
+							<div class="timeline-content">
+							<p>'.$education_type_description.'</p>
+							</div>
+						</div>
+					</div>';
+				}
+			?>
 			<div class="col-md-6">
 			  <div class="timeline-box">
 				<div class="time-line-header">
@@ -564,42 +510,40 @@ $contact_website = get_post_meta( get_the_ID(), 'contact_website', true);
 			?>
 		  <h3 class="subtitle"><?php echo $experience_title;?></h3>
 		  <div class="row vertical-line">
-		  <?php 
-		$experience_types = get_post_meta( get_the_ID(), 'experience_type', true );
-		foreach ( (array) $experience_types as $key => $experience_type ) {
-			$experience_position = $experience_institution = $experience_type_description = $education_year = $institution_website = '';
-			if ( isset( $experience_type['experience_position'] ) ) {
-				$experience_position = esc_html( $experience_type['experience_position'] );
-			}
-			if ( isset( $experience_type['experience_institution'] ) ) {
-				$experience_institution = esc_html( $experience_type['experience_institution'] );
-			}
-			if ( isset( $experience_type['experience_year'] ) ) {
-				$experience_year = esc_html( $experience_type['experience_year']);
-			}
-			if ( isset( $experience_type['experience_description'] ) ) {
-				$experience_type_description = esc_html( $experience_type['experience_description']);
-			}
-			if ( isset( $experience_type['institution_website'] ) ) {
-				$institution_website = esc_html( $experience_type['institution_website']);
-			}
-			
-		
-			echo '
-			<div class="col-md-6">
-			<div class="timeline-box">
-			  <div class="time-line-header">
-				<p class="timeline-year">'.$experience_year.'</p>
-				<h6 class="timeline-title">'.$experience_position.'</h6>
-				<p class="timeline-title">Company: <a href="'.$institution_website.'" target="_blank">'.$experience_institution.'</a></p>
-			  </div>
-			  <div class="timeline-content">
-				<p>'.$experience_type_description.'</p>
-			  </div>
-			</div>
-			</div>';
-		}
-	  ?>
+		  	<?php 
+				$experience_types = get_post_meta( get_the_ID(), 'experience_type', true );
+				foreach ( (array) $experience_types as $key => $experience_type ) {
+					$experience_position = $experience_institution = $experience_type_description = $education_year = $institution_website = '';
+					if ( isset( $experience_type['experience_position'] ) ) {
+						$experience_position = esc_html( $experience_type['experience_position'] );
+					}
+					if ( isset( $experience_type['experience_institution'] ) ) {
+						$experience_institution = esc_html( $experience_type['experience_institution'] );
+					}
+					if ( isset( $experience_type['experience_year'] ) ) {
+						$experience_year = esc_html( $experience_type['experience_year']);
+					}
+					if ( isset( $experience_type['experience_description'] ) ) {
+						$experience_type_description = esc_html( $experience_type['experience_description']);
+					}
+					if ( isset( $experience_type['institution_website'] ) ) {
+						$institution_website = esc_html( $experience_type['institution_website']);
+					}
+					echo '
+					<div class="col-md-6">
+						<div class="timeline-box">
+							<div class="time-line-header">
+								<p class="timeline-year">'.$experience_year.'</p>
+								<h6 class="timeline-title">'.$experience_position.'</h6>
+								<p class="timeline-title">Company: <a href="'.$institution_website.'" target="_blank">'.$experience_institution.'</a></p>
+							</div>
+							<div class="timeline-content">
+								<p>'.$experience_type_description.'</p>
+							</div>
+						</div>
+					</div>';
+				}
+			?>
 			<!--   Item 01   -->
 			<div class="col-md-6">
 			  <div class="timeline-box">
@@ -690,47 +634,46 @@ $contact_website = get_post_meta( get_the_ID(), 'contact_website', true);
 	  </div>
 	  <div class="portfolio-items border-line-v row">
 	  <?php 
-$args = array(
-    'post_type' => 'portfolio', 
-);
-$query = new WP_Query($args);
+		$args = array(
+			'post_type' => 'portfolio', 
+		);
+		$query = new WP_Query($args);
 
-if ($query->have_posts()) {
-    while ($query->have_posts()) {
-        $query->the_post();
-        
-        $portfolio_link = get_post_meta(get_the_ID(), 'portfolio_link', true );
+		if ($query->have_posts()) {
+			while ($query->have_posts()) {
+				$query->the_post();
+				
+				$portfolio_link = get_post_meta(get_the_ID(), 'portfolio_link', true );
 
-        // Get the terms associated with the current post, excluding the "Uncategorized" category
-        $terms = get_the_terms(get_the_ID(), 'category');
+				// Get the terms associated with the current post, excluding the "Uncategorized" category
+				$terms = get_the_terms(get_the_ID(), 'category');
 
-        if (!empty($terms)) {
-            // Filter out the "Uncategorized" term
-            $filtered_terms = array_filter($terms, function ($term) {
-                return $term->slug !== 'uncategorized';
-            });
+				if (!empty($terms)) {
+					// Filter out the "Uncategorized" term
+					$filtered_terms = array_filter($terms, function ($term) {
+						return $term->slug !== 'uncategorized';
+					});
 
-            foreach ($filtered_terms as $term) {
-                echo '<div class="col-md-6 col-lg-4 portfolio-item ' . $term->slug . '">
-                            <div class="portfolio-box">
-                                <div class="portfolio-image">' . get_the_post_thumbnail(get_the_ID(), 'post_image_xl', array('class' => 'alignleft')) . '
-                                    <div class="portfolio-icon">
-                                        <a href="' . $portfolio_link . '" class="mfp-iframe" target="_blank">
-                                            <i class="bi bi-link"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="portfolio-content">
-                                    <h6 class="blog-header"><a href="' . $portfolio_link . '" class="mfp-iframe" target="_blank">' . get_the_title() . '</a></h6>
-                                </div>
-                            </div>
-                        </div>';
-            }
-        }
-    }
-}
-?>
-
+					foreach ($filtered_terms as $term) {
+						echo '<div class="col-md-6 col-lg-4 portfolio-item ' . $term->slug . '">
+									<div class="portfolio-box">
+										<div class="portfolio-image">' . get_the_post_thumbnail(get_the_ID(), 'post_image_xl', array('class' => 'alignleft')) . '
+											<div class="portfolio-icon">
+												<a href="' . $portfolio_link . '" class="mfp-iframe" target="_blank">
+													<i class="bi bi-link"></i>
+												</a>
+											</div>
+										</div>
+										<div class="portfolio-content">
+											<h6 class="blog-header"><a href="' . $portfolio_link . '" class="mfp-iframe" target="_blank">' . get_the_title() . '</a></h6>
+										</div>
+									</div>
+								</div>';
+					}
+				}
+			}
+		}
+		?>
 	</div>
   </section>
   <!--  Portfolio End  -->
@@ -741,45 +684,42 @@ if ($query->have_posts()) {
 	  <h3 class="subtitle">Latest News</h3>
 	  <div class="boxes">
 		<div class="row vertical-line" id="append-here">
-		<?php 
-		$args = array(
-				'post_type' => 'post', 
-				'posts_per_page' => '2',
-			);
-			$query = new WP_Query($args);
-
-			if ($query->have_posts()) {
-				while ($query->have_posts()) {
-					$query->the_post();
-		  echo '<div class="col-md-6">
-			<a href="' . get_the_permalink() . '" class="blog-box">
-			  <div class="blog-image">
-			  ' . get_the_post_thumbnail(get_the_ID(), 'post_image_xl', array('class' => 'alignleft')) . '
-				<div class="blog-icon">
-				  <i class="bi bi-journal-text"></i>
-				</div>
-			  </div>
-			  <div class="blog-post-content">
-			  	<h6 class="blog-header">' . get_the_title() . '</h6>
-				<div class="blog-dates">
-				  <span>' . get_the_date() . '</span>
-				</div>
-				<p class="mb-0">' . get_the_excerpt() . '</p>
-			  </div>
-			</a>
-		  </div>';
+			<?php 
+				$args = array(
+						'post_type' => 'post', 
+						'posts_per_page' => '2',
+					);
+				$query = new WP_Query($args);
+				if ($query->have_posts()) {
+					while ($query->have_posts()) {
+						$query->the_post();
+						echo '<div class="col-md-6">
+							<a href="' . get_the_permalink() . '" class="blog-box">
+							<div class="blog-image">
+							' . get_the_post_thumbnail(get_the_ID(), 'post_image_xl', array('class' => 'alignleft')) . '
+								<div class="blog-icon">
+								<i class="bi bi-journal-text"></i>
+								</div>
+							</div>
+							<div class="blog-post-content">
+								<h6 class="blog-header">' . get_the_title() . '</h6>
+								<div class="blog-dates">
+								<span>' . get_the_date() . '</span>
+								</div>
+								<p class="mb-0">' . get_the_excerpt() . '</p>
+							</div>
+							</a>
+						</div>';
+					}
 				}
-			}
-			wp_reset_postdata(); // Restore original post data
-		  ?>
+				wp_reset_postdata(); // Restore original post data
+			?>
 		</div>
 	  </div>
-	  
-		  
-	  <div class="load-more-containerv text-center">
-                                <button id="load-more-news" class="primary-button">View More Posts</button>
-                                <p id="fully-loaded">That's all for now &#128522.</p>
-                            </div>
+		<div class="load-more-containerv text-center">
+			<button id="load-more-news" class="primary-button">View More Posts</button>
+			<p id="fully-loaded">That's all for now &#128522.</p>
+		</div>
 	</div>
   </section>
   <!--  Blog End  -->
@@ -800,7 +740,7 @@ if ($query->have_posts()) {
 	  <div class="row mt-4">
 		<!-- Address Info -->
 		<div class="col-12 col-xl-5">
-	  <p id="fully-loaded" style="text-align: justify;"><?php echo $contact_description;?></p>
+	  		<p id="fully-loaded" style="text-align: justify;"><?php echo $contact_description;?></p>
 		  <div class="row">
 			<!--  Item 01 -->
 			<div class="col-lg-12">
